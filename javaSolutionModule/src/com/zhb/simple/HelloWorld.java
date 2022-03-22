8788ff6f4195c761178146438f6da36be327d6d9
zhb <18851822250@163.com>
2022-03-23 01:51:10 +0800

    zhb:HelloWorld +:HelloWorld.java

---
+++ javaSolutionModule/src/com/zhb/simple/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zhb.simple;
+
+/**
+ * @author zhb
+ * @create 2022-03-23 1:49
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+}
