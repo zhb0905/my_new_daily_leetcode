f817db6ce98f83b38a3ea39ade155b6234fe3518
zhb <18851822250@163.com>
2022-03-23 22:40:13 +0800

    zhb:README.md +:README.md +:Problem_26_RemoveDuplicates.java

---
+++ javaSolutionModule/src/com/zhb/simple/from1to50/Problem_26_RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package com.zhb.simple.from1to50;
+
+/**
+ * @author zhb
+ * @create 2022-03-23 22:14
+ */
+
+/**
+ * 26. 删除有序数组中的重复项 (原数组升序排列，要求：原地)
+ * 标签 ：
+ */
+public class Problem_26_RemoveDuplicates {
+    class Solution {
+        //快慢指针。 慢：左边是答案数组。快：遍历（leftBiggest:快指针左边的最大值）
+        public int removeDuplicates(int[] nums) {
+
+            for (int i = 0; i < nums.length; i++) {
+                //todo:快慢指针
+                ;
+            }
+            return 0;
+        }
+    }
+}
