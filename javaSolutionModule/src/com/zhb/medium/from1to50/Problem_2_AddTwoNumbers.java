baf2d53d3b7e05306af59cb5302fd6f414683afe
zhb <18851822250@163.com>
2022-03-24 20:49:35 +0800

    zhb:Problem_2_AddTwoNumbers debug记录 *:Problem_2_AddTwoNumbers.java

--- javaSolutionModule/src/com/zhb/medium/from1to50/Problem_2_AddTwoNumbers.java
+++ javaSolutionModule/src/com/zhb/medium/from1to50/Problem_2_AddTwoNumbers.java
@@ -24,20 +24,29 @@ import org.junit.Test;
 
 /**
  * BigInteger
+ * debug记录 ：
+ * 一：超出内存限制 。 debug:写漏 l1 = l1.next;l2 = l2.next;
+ * 二：WRONG 14 / 1568 个通过测试用例 。 debug：最后的if笔误写成了carryBit == 1
+ * 三：WRONG 46 / 1568 个通过测试用例。debug：tempSum写漏了carryBit
+ * 四：AC
  */
 public class Problem_2_AddTwoNumbers {
+
+    Solution solution = new Solution();
     class Solution {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {// [2,4,3] [5,6,4]
             ListNode sentry = new ListNode(-1, null); //哨兵
             ListNode pointer = sentry;
             int carryBit = 0;
             ListNode toAddNode = null;
             while(l1!= null && l2!=null ) {
-                int tempSum = l1.val+ l2.val ;
+                int tempSum = l1.val+ l2.val + carryBit ;
                 carryBit = tempSum / 10;
                 toAddNode = new ListNode(tempSum % 10 ,null);
                 pointer.next = toAddNode;
                 pointer = toAddNode;
+                l1 = l1.next;
+                l2 = l2.next;
             }
             if(l1 == null) {
                 /*if(carryBit == 0) {
@@ -49,11 +58,12 @@ public class Problem_2_AddTwoNumbers {
                         pointer.next = l2 ;
                         return sentry.next;
                     }
-                    int tempSum = 1 + l2.val ;
+                    int tempSum = carryBit + l2.val ;
                     carryBit = tempSum / 10;
                     toAddNode = new ListNode(tempSum % 10 ,null);
                     pointer.next = toAddNode;
                     pointer = toAddNode;
+                    l2 = l2.next;
 
                 }
             }
@@ -68,21 +78,36 @@ public class Problem_2_AddTwoNumbers {
                         pointer.next = l1 ;
                         return sentry.next;
                     }
-                    int tempSum = 1 + l1.val ;
+                    int tempSum = carryBit + l1.val ;
                     carryBit = tempSum / 10;
                     toAddNode = new ListNode(tempSum % 10 ,null);
                     pointer.next = toAddNode;
                     pointer = toAddNode;
-
+                    l1 = l1.next;
                 }
             }
 
-             if(carryBit == 0) {
-                    pointer.next =new ListNode(1 ,null);
+             if(carryBit == 1) {
+                    pointer.next =new ListNode(carryBit ,null);
                     return sentry.next;
                 }
             return sentry.next;
         }
     }
 
+
+    @Test
+    public void test1(){
+       // [2,4,3] [5,6,4]
+        ListNode l13 = new ListNode(3,null);
+        ListNode l12 = new ListNode(4,l13);
+        ListNode l11 = new ListNode(2,l12);
+
+        ListNode l23 = new ListNode(4,null);
+        ListNode l22 = new ListNode(6,l23);
+        ListNode l21 = new ListNode(5,l22);
+
+        ListNode listNode = solution.addTwoNumbers(l11, l21);
+        System.out.println(listNode.val+"\t" +listNode.next.val+"\t" + listNode.next.next.val);
+    }
 }
