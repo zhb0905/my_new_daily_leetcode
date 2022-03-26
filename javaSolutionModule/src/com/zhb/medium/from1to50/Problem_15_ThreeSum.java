b4e349b0a20b93ac9ac3b1a2fdc4f3c69c465eee
zhb <18851822250@163.com>
2022-03-26 22:59:43 +0800

    zhb: Problem_15_ThreeSum  思路 *: Problem_15_ThreeSum.java

--- javaSolutionModule/src/com/zhb/medium/from1to50/Problem_15_ThreeSum.java
+++ javaSolutionModule/src/com/zhb/medium/from1to50/Problem_15_ThreeSum.java
@@ -4,5 +4,24 @@ package com.zhb.medium.from1to50;
  * @author zhb
  * @create 2022-03-25 1:11
  */
+
+import java.util.List;
+
+/**
+ * 15. 三数之和
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ */
 public class Problem_15_ThreeSum {
+    /**
+     * 思路：
+     * 两层循环得到: <sum,List<num1, num2> >
+     * 一次遍历找 -sum；
+     */
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            return null;
+        }
+    }
 }
