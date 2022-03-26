aa04e15fce8d707a705489f2082430c47215b4d5
zhb <18851822250@163.com>
2022-03-26 23:21:46 +0800

    zhb: Problem_19_RemoveNthFromEnd  思路 *: Problem_19_RemoveNthFromEnd.java

---
+++ javaSolutionModule/src/com/zhb/medium/from1to50/Problem_19_RemoveNthFromEnd.java
@@ -0,0 +1,25 @@
+package com.zhb.medium.from1to50;
+
+/**
+ * @author zhb
+ * @create 2022-03-26 23:17
+ */
+
+import com.zhb.mylocallib.ListNode;
+
+/**
+ * 19. 删除链表的倒数第 N 个结点
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ */
+public class Problem_19_RemoveNthFromEnd {
+    /**
+     * 思路：
+     * 距离为 n 的双指针
+     */
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            return null;
+        }
+    }
+}
