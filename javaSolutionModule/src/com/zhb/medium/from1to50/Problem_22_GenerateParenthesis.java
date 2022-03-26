ef81b953b841e306ec78cd6dbac571f40a3788dc
zhb <18851822250@163.com>
2022-03-26 23:29:24 +0800

    zhb: Problem_22_GenerateParenthesis  思路 火车进站 *: Problem_22_GenerateParenthesis.java

---
+++ javaSolutionModule/src/com/zhb/medium/from1to50/Problem_22_GenerateParenthesis.java
@@ -0,0 +1,23 @@
+package com.zhb.medium.from1to50;
+
+/**
+ * @author zhb
+ * @create 2022-03-26 23:27
+ */
+
+import java.util.List;
+
+/**
+ * 22. 括号生成
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ */
+public class Problem_22_GenerateParenthesis {
+    /**
+     * 思路：“火车进站” 问题 。 push:(  pop:)
+     */
+    class Solution {
+        public List<String> generateParenthesis(int n) {
+            return null;
+        }
+    }
+}
